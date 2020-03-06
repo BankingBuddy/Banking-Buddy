48e745320bab051c23960418bf9fce3375b5b50d
Calvin Tsang <tsangche@gmail.com>
2020-03-06 10:07:32 +0000

    updated classes

---
+++ bankingbuddy/src/Goals.java
@@ -0,0 +1,17 @@
+public class Goals {
+
+    private String goalName;
+
+    private double goalAmount;
+
+    Goals(){
+
+    }
+    private void addGoal(){
+
+    }
+
+    public String getGoalName() {
+        return goalName;
+    }
+}
