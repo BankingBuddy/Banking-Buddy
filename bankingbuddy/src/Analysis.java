91b6f2f43df76d492185ea693f545c3807921c2b
Bentopher <xx1ben99xx@gmail.com>
2020-04-17 01:35:37 +0100

    Implemented basic charts

--- bankingbuddy/src/Analysis.java
+++ bankingbuddy/src/Analysis.java
@@ -1,5 +1,6 @@
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.block.BlockBorder;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
@@ -14,29 +15,29 @@ import java.awt.*;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class Analysis {
     public XYDataset createLineDataset(ArrayList<Entry> entries){
-        XYSeries series1 = new XYSeries("Food");
-        series1.add(10, 500);
-        series1.add(50, 200);
-        series1.add(100, 300);
-
-        XYSeries series2 = new XYSeries("Drink");
-        series2.add(32, 500);
-        series2.add(65, 340);
-        series2.add(120, 340);
-
+        XYSeries expenditureSeries = new XYSeries("Expenditure");
+        XYSeries incomeSeries = new XYSeries("Income");
+        for (Entry entry : entries){
+            if (entry.getType().equals(Entry.Type.Expenditure)){
+                expenditureSeries.add(entry.getTimeStamp().getTime(), entry.getAmount());
+            }else{
+                incomeSeries.add(entry.getTimeStamp().getTime(), entry.getAmount());
+            }
+        }
         XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(series1);
-        dataset.addSeries(series2);
+        dataset.addSeries(expenditureSeries);
+        dataset.addSeries(incomeSeries);
         return dataset;
     }
 
     public JFreeChart createLineChart(ArrayList<Entry> entries){
         XYDataset dataset = createLineDataset(entries);
-        JFreeChart chart = ChartFactory.createXYLineChart("My Chart", "x", "y", dataset, PlotOrientation.VERTICAL, true, true, false);
+        JFreeChart chart = ChartFactory.createTimeSeriesChart("Spending's", "Date", "Amount (£)", dataset, true, true, false);
 
         XYPlot plot = chart.getXYPlot();
 
@@ -53,34 +54,35 @@ public class Analysis {
         plot.setDomainGridlinesVisible(false);
 
         chart.getLegend().setFrame(BlockBorder.NONE);
-        chart.setTitle(new TextTitle("My Chart"));
+        chart.setTitle(new TextTitle("Spending's"));
         return chart;
     }
 
     public DefaultPieDataset createPieDataset(ArrayList<Entry> entries){
         BigDecimal totalAmount = new BigDecimal(BigInteger.ZERO);
-        ArrayList<AnalysisCategory> cumulativeCategories = new ArrayList<>();
+        ArrayList<Entry> cumulativeEntries = new ArrayList<>();
         for (Entry entry : entries){
             totalAmount = totalAmount.add(entry.getAmount());
             boolean contains = false;
-            for (AnalysisCategory category : cumulativeCategories){
-                if (category.getCategoryName().equals(entry.getTransactionCategory().getCategoryName())){
-                    category.setAmount(category.getAmount().add(entry.getAmount()));
+            for (Entry cumEntry : cumulativeEntries){
+                if (cumEntry.getTransactionCategory().getCategoryName().equals(entry.getTransactionCategory().getCategoryName())){
+                    cumEntry.setAmount(cumEntry.getAmount().add(entry.getAmount()));
                     contains = true;
                     break;
                 }
             }
             if (!contains){
-                AnalysisCategory newAnalysisCategory = new AnalysisCategory(entry.getTransactionCategory().getCategoryName());
-                newAnalysisCategory.setAmount(entry.getAmount());
-                cumulativeCategories.add(newAnalysisCategory);
+                Entry newEntry = new Entry();
+                newEntry.setTransactionCategory(entry.getTransactionCategory());
+                newEntry.setAmount(entry.getAmount());
+                cumulativeEntries.add(newEntry);
             }
         }
 
         DefaultPieDataset dataset = new DefaultPieDataset();
         if (!totalAmount.equals(BigDecimal.ZERO)){
-            for (AnalysisCategory category : cumulativeCategories){
-                dataset.setValue(category.getCategoryName(), category.getAmount().multiply(new BigDecimal(100).divide(totalAmount, RoundingMode.CEILING)).intValueExact());
+            for (Entry entry : cumulativeEntries){
+                dataset.setValue(entry.getTransactionCategory().getCategoryName(), entry.getAmount().multiply(new BigDecimal(100).divide(totalAmount, RoundingMode.CEILING)).intValueExact());
             }
         }
         return dataset;
@@ -88,6 +90,8 @@ public class Analysis {
 
     public JFreeChart createPieChart(ArrayList<Entry> entries){
         DefaultPieDataset dataset = createPieDataset(entries);
-        return ChartFactory.createPieChart("Categories", dataset, false, true, false);
+        JFreeChart chart = ChartFactory.createPieChart("Categories", dataset, false, true, false);
+        chart.getPlot().setBackgroundPaint(Color.white);
+        return chart;
     }
 }
